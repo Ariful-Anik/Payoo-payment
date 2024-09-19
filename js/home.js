3c81188d8cad0daf7dcab3a927336dad62fec97c
Ariful Anik <arifulanik2021@gmail.com>
2024-09-19 15:31:50 +0600

    added

--- js/home.js
+++ js/home.js
@@ -6,6 +6,20 @@ document.getElementById("btn-add-money").addEventListener('click', function(e){
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(addMoneyInput,pinNumberInput);
 
+    if(pinNumberInput === '1234'){
+        console.log("adding money");
+        const balance = document.getElementById('account-balance').innerText;
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+        document.getElementById('account-balance').innerText= newBalance;
+
+    }
+    else{
+        alert("Wrong");
+    }
+
 
 
 })
